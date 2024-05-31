cdb63a870be6982ca91f1e103ea16d3156fbe801
Alex Kennedy <alexbkennedy96@gmail.com>
2024-05-31 19:43:06 -0400

    Add streaming functionality for dashboard components

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -17,6 +17,9 @@ export async function fetchRevenue() {
   noStore();
 
   try {
+    // Artificially delay a response for demo purposes.
+    await new Promise((resolve) => setTimeout(resolve, 3000));
+
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
     return data.rows;
@@ -29,6 +32,9 @@ export async function fetchRevenue() {
 export async function fetchLatestInvoices() {
   noStore();
   try {
+    // Artificially delay a response for demo purposes.
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
       FROM invoices
@@ -53,6 +59,10 @@ export async function fetchCardData() {
     // You can probably combine these into a single SQL query
     // However, we are intentionally splitting them to demonstrate
     // how to initialize multiple queries in parallel with JS.
+
+    // Artificially delay a response for demo purposes.
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+
     const invoiceCountPromise = sql`SELECT COUNT(*) FROM invoices`;
     const customerCountPromise = sql`SELECT COUNT(*) FROM customers`;
     const invoiceStatusPromise = sql`SELECT
